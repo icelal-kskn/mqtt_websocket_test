06f6327e4b49e2e3cfa9ded912b94540a17cbbbd
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2024-03-05 16:28:56 +0300

    Mqtt subscribe is working

--- static/index.js
+++ static/index.js
@@ -1,58 +1,73 @@
-function connectOnClick(){
-    
-    const clientId= 'mqttjs_' +parseInt(Math.random() * 100);
+let client;
 
+function connectOnClick() {
+    const clientId = 'mqttjs_' + parseInt(Math.random() * 100);
 
-    const host=document.getElementById("host").value;
-    const port=document.getElementById("port").value;
-    const username=document.getElementById("username").value;
-    const password=document.getElementById("password").value;
+    let host = document.getElementById("host").value;
+    const port = document.getElementById("port").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    const client= mqtt.Client(
-        [
-            { 
-                host: host,
-                port: port, 
-                clientId: clientId }] );
-
-};
-
-function onConnect(){
-    topic= document.getElementById("topic_s").value;
-    
-    document.getElementById("messages").innerHTML += "<span> Subscribing the Topic:" + topic  +" ... </span><br>"
-    
-    client.subscribe(topic)
-};
-
-
-function onConnectionLost(responseObject){
-    document.getElementById("messages").innerHTML += "<span> ERROR: Connection Lost ... </span><br>"
-    if(responseObject.errorMessage) {
-        document.getElementById("messages").innerHTML += "<span> ERROR:"+ responseObject.errorMessage + "</span><br>";
+    if (!host.startsWith('wss://')) {
+        host = 'wss://' + host;
     }
-};
-function onMessageArrived(message){
-    console.log("onMessageArrived called: " +message.payloadString);
-    document.getElementById("messages").innerHTML += "<span> Topic: "+ message.destinationName+"| Message: "+message.payloadString + "</span><br>";
-};
 
-function disconnectOnClick(){
-    if (client.isConnected()) {
+    client = mqtt.connect(`${host}:${port}`, {
+        clientId: clientId,
+        username: username || null,
+        password: password || null,
+    });
+
+    document.getElementById("output").innerHTML += "<span>Connecting the client: " + host + " ... </span><br>";
+    document.getElementById("output").innerHTML += "<span>On Port:" + port + " ... </span><br>";
+    
+    client.on('connect', onConnect);
+    client.on('error', onConnectError);
+    client.on('close', onConnectionLost);
+}
+
+function onConnect() {
+    const topic = document.getElementById("topic_s").value;
+
+    document.getElementById("output").innerHTML += "<span> Subscribing the Topic: " + topic + " ... </span><br>";
+
+    client.subscribe(topic);
+    console.log(client)
+}
+
+function onConnectError(err) {
+    console.log("Connection error", err);
+}
+
+function onConnectionLost(responseObject) {
+    document.getElementById("output").innerHTML += "<span> ERROR: Connection Lost ... </span><br>";
+    if (responseObject.errorMessage) {
+        document.getElementById("output").innerHTML += "<span> ERROR:" + responseObject.errorMessage + "</span><br>";
+    }
+}
+
+function onMessageArrived(message) {
+    console.log("onMessageArrived called: " + message.payloadString);
+    document.getElementById("output").innerHTML += "<span> Topic: " + message.destinationName + "| Message: " + message.payloadString + "</span><br>";
+}
+
+function disconnectOnClick() {
+    if (client && client.isConnected()) {
         client.disconnect();
-        document.getElementById("messages").innerHTML += "<span> Disconnected successfully</span><br>";
+        document.getElementById("output").innerHTML += "<span> Disconnected successfully</span><br>";
     } else {
-        document.getElementById("messages").innerHTML += "<span> Client is not connected</span><br>";
+        document.getElementById("output").innerHTML += "<span> Client is not connected</span><br>";
     }
-};
+}
 
-function  publishOnClick() {
-    topic = document.getElementById("topc_p").value;
+function publishOnClick() {
+    const topic = document.getElementById("topc_p").value;
+    const message = document.getElementById("message").value;
 
-    if (client.isConnected()) {
+    if (client) {
         client.publish(topic, message);
-        document.getElementById("messages").innerHTML += "<span> Published to Topic: " + topic + " | Message: " + message + "</span><br>";
+        document.getElementById("output").innerHTML += "<span> Published to Topic: " + topic + " | Message: " + message + "</span><br>";
     } else {
-        document.getElementById("messages").innerHTML += "<span> Client is not connected</span><br>";
+        document.getElementById("output").innerHTML += "<span> Client is not connected</span><br>";
     }
-};
+}
